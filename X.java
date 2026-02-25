15fc84cfde3686cc340d8414bcf6f831803db870
Abednego254 <kaumeabednego3813@gmail.com>
2026-02-25 14:47:21 +0300

    Must pass java assessment

--- X.java
+++ X.java
@@ -1,3 +1,14 @@
-public interface X {
-    int VALUE = 10;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class X {
+    public static void main(String [] args) {
+        List<String> names = Arrays.asList("John", "Alice", "Aliz", "Bob", "Alice");
+        Collections.sort(names);
+        System.out.println(names);
+    }
 }
--- Y.java
+++ Y.java
@@ -1,5 +1,5 @@
-public class Y implements X {
-    public static void main(String[] args) {
-        System.out.println(VALUE);
-    }
-}
+//public class Y implements X {
+//    public static void main(String[] args) {
+//        System.out.println(VALUE);
+//    }
+//}
