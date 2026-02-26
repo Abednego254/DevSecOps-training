be6ddad46a3aabb3eeff0d3ef613c28b19e0a864
Abednego254 <kaumeabednego3813@gmail.com>
2026-02-26 09:06:21 +0300

    Food for thought

---
+++ javaSE/Main.java
@@ -0,0 +1,12 @@
+package javaSE;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(LocalTime.now());
+        char [] chars = {'a', 'b', 'c',};
+
+        System.out.println(chars.getClass().getName());
+    }
+}
